01ed4cc8ec112cb0141fab2823d6bb8cd6e121e2
Kirill Warp <kirill9990@gmail.com>
2024-06-29 00:20:33 +0300

    make more informative Not Implemented screen

--- src/gui/scenes/NotImplemented.ts
+++ src/gui/scenes/NotImplemented.ts
@@ -1,29 +1,21 @@
-import { Scene, Label, Color, vec } from 'excalibur';
-import { ActorEvents } from '../events/ActorEvents';
+import { Scene, Label, Color, vec, BaseAlign } from 'excalibur';
 import { GameEvent, GameEngine } from '../GameEngine';
 import { BackButton } from '../kit/BackButton';
 import { PixelFont30px } from '../PrepareFonts';
 
 export class NotImplementedScene extends Scene {
   public onInitialize(engine: GameEngine) {
-    let prevTextWidth = 0;
     const text = new Label({
-      text: 'Not Implemented!',
-      pos: engine.screen.center.sub(vec(0, 100)),
+      text: `${engine.currentSceneName} is Not Implemented!`,
+      pos: engine.screen.center,
       color: Color.White,
       font: PixelFont30px({
+        baseAlign: BaseAlign.Middle,
         shadow: {
           color: Color.Black,
         }
       }),
     });
-
-    text.events.on(ActorEvents.PostDraw, () => {
-      if (prevTextWidth !== text.getTextWidth()) {
-        prevTextWidth = text.getTextWidth();
-        text.pos.x = engine.screen.center.x - text.getTextWidth() / 2;
-      }
-    })
 
     this.add(text);
 
