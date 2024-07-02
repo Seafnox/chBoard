2e31eb5087b7969dae0a0e7ff5f2071e70341216
Kirill Warp <kirill9990@gmail.com>
2024-07-02 16:48:28 +0300

    fix border drawng in Ch*Board

--- src/gui/kit/CheckersBoard.ts
+++ src/gui/kit/CheckersBoard.ts
@@ -1,10 +1,11 @@
-import { ScreenElement, Rectangle, GraphicsGroup, Color, vec, Text } from 'excalibur';
+import { ScreenElement, Rectangle, GraphicsGroup, Color, vec, Text, Vector } from 'excalibur';
 import { GraphicsGrouping } from 'excalibur/build/dist/Graphics/GraphicsGroup';
 import { GameConfig } from '../../engine/GameConfig';
 import { PixelFont30px } from '../PrepareFonts';
 
-const cellSize = 50;
-const borderSize = 30;
+const cellSize = 80;
+const borderSize = 50;
+const borderBorderCoef = 0.1;
 const graphicState = 'root';
 const lightColor = Color.fromHex('DBC1A2');
 const darkColor = Color.fromHex('4F3B2C');
@@ -13,10 +14,12 @@ export class CheckersBoard extends ScreenElement {
 
   constructor(
     public readonly initialConfig: GameConfig<unknown, unknown, unknown>,
+    offset: Vector,
   ) {
     super({
       width: cellSize * initialConfig.width + borderSize * 2,
       height: cellSize * initialConfig.height + borderSize * 2,
+      offset,
     });
 
     this.graphics.add(graphicState, this.getStateGroup(initialConfig));
@@ -30,47 +33,58 @@ export class CheckersBoard extends ScreenElement {
     return new GraphicsGroup({
       members: [
         {
+          graphic: this.getHorizontalBorder(initialConfig, true),
+          offset: vec(-this.offset.x / 2, 0),
+        },
+        {
+          graphic: this.getVerticalBorder(initialConfig, true),
+          offset: vec(-this.offset.x / 2, 0),
+        },
+        {
           graphic: this.getHorizontalBorder(initialConfig),
-          offset: vec(borderSize, borderSize),
+          offset: vec(-this.offset.x / 2, initialConfig.height * cellSize + borderSize),
         },
         {
           graphic: this.getVerticalBorder(initialConfig),
-          offset: vec(borderSize, borderSize),
-        },
-        {
-          graphic: this.getHorizontalBorder(initialConfig),
-          offset: vec(borderSize, borderSize + initialConfig.height * cellSize),
-        },
-        {
-          graphic: this.getVerticalBorder(initialConfig),
-          offset: vec(borderSize + initialConfig.width * cellSize, borderSize),
+          offset: vec(initialConfig.width * cellSize + borderSize-this.offset.x / 2, 0),
         },
         {
           graphic: this.getCells(initialConfig),
-          offset: vec(borderSize, borderSize),
+          offset: vec(borderSize-this.offset.x / 2, borderSize),
         },
       ],
     });
   }
 
-  private getVerticalBorder(initialConfig: GameConfig<unknown, unknown, unknown>): GraphicsGroup {
+  private getVerticalBorder(
+    initialConfig: GameConfig<unknown, unknown, unknown>,
+    isLeftBorder = false,
+  ): GraphicsGroup {
     return new GraphicsGroup({
       members: [
         {
           graphic: new Rectangle({
             width: borderSize,
-            height: initialConfig.height * cellSize + borderSize * 2,
+            height: initialConfig.height * cellSize + borderSize * 2 * (1 - borderBorderCoef),
             color: lightColor,
           }),
-          offset: vec(0, 0),
+          offset: vec(0, borderSize * borderBorderCoef),
         },
         {
           graphic: new Rectangle({
-            width: borderSize / 10,
+            width: borderSize * borderBorderCoef,
             height: initialConfig.height * cellSize + borderSize * 2,
             color: darkColor,
           }),
-          offset: vec(borderSize, borderSize),
+          offset: vec(isLeftBorder ? 0 : borderSize * (1 - borderBorderCoef), 0),
+        },
+        {
+          graphic: new Rectangle({
+            width: borderSize * borderBorderCoef,
+            height: initialConfig.height * cellSize + borderSize * 2 * borderBorderCoef,
+            color: darkColor,
+          }),
+          offset: vec(isLeftBorder ? borderSize * (1 - borderBorderCoef) : 0, borderSize * (1 - borderBorderCoef)),
         },
         ...this.getVerticalBorderNumbers(initialConfig),
       ],
@@ -88,24 +102,35 @@ export class CheckersBoard extends ScreenElement {
     }));
   }
 
-  private getHorizontalBorder(initialConfig: GameConfig<unknown, unknown, unknown>): GraphicsGroup {
+  private getHorizontalBorder(
+    initialConfig: GameConfig<unknown, unknown, unknown>,
+    isTopBorder = false,
+  ): GraphicsGroup {
     return new GraphicsGroup({
       members: [
         {
           graphic: new Rectangle({
-            width: initialConfig.width * cellSize + borderSize * 2,
+            width: initialConfig.width * cellSize + borderSize * 2 * (1 - borderBorderCoef),
             height: borderSize,
             color: lightColor,
           }),
-          offset: vec(0, 0),
+          offset: vec(borderSize * borderBorderCoef, 0),
         },
         {
           graphic: new Rectangle({
             width: initialConfig.width * cellSize + borderSize * 2,
-            height: borderSize / 10,
+            height: borderSize * borderBorderCoef,
             color: darkColor,
           }),
-          offset: vec(borderSize, borderSize),
+          offset: vec(0, isTopBorder ? 0 : borderSize * (1 - borderBorderCoef)),
+        },
+        {
+          graphic: new Rectangle({
+            width: initialConfig.width * cellSize + borderSize * 2 * borderBorderCoef,
+            height: borderSize * borderBorderCoef,
+            color: darkColor,
+          }),
+          offset: vec(borderSize * (1 - borderBorderCoef), isTopBorder ? borderSize * (1 - borderBorderCoef) : 0),
         },
         ...this.getHorizontalBorderLetters(initialConfig),
       ],
@@ -125,15 +150,21 @@ export class CheckersBoard extends ScreenElement {
   }
 
   private getCells(initialConfig: GameConfig<unknown, unknown, unknown>): GraphicsGroup {
+    const cellMembers: GraphicsGrouping[] = [];
+    for (let y = 0; y < initialConfig.height; y++) {
+      for (let x = 0; x < initialConfig.width; x++) {
+        cellMembers.push({
+          graphic: new Rectangle({
+            width: cellSize,
+            height: cellSize,
+            color: (x + y) % 2 === 0 ? lightColor : darkColor,
+          }),
+          offset: vec(x * cellSize, y * cellSize),
+        });
+      }
+    }
     return new GraphicsGroup({
-      members: Array(initialConfig.width * initialConfig.height).fill(0).map<GraphicsGrouping>((_, index) => ({
-        graphic: new Rectangle({
-          width: cellSize,
-          height: cellSize,
-          color: initialConfig.cellMap[index] === 'light' ? lightColor : darkColor,
-        }),
-        offset: vec((index % initialConfig.width) * cellSize + borderSize, Math.floor(index / initialConfig.width) * cellSize + borderSize),
-      })),
+      members: cellMembers,
     });
   }
 }
