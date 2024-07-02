339b15a7e1e674f64cb17376c1d3ee59dc33f5a6
Kirill Warp <kirill9990@gmail.com>
2024-07-02 15:57:59 +0300

    fix semicolons and etc

--- src/gui/kit/CheckersBoard.ts
+++ src/gui/kit/CheckersBoard.ts
@@ -12,7 +12,7 @@ const darkColor = Color.fromHex('4F3B2C');
 export class CheckersBoard extends ScreenElement {
 
   constructor(
-    public readonly initialConfig: GameConfig<any, any, any>,
+    public readonly initialConfig: GameConfig<unknown, unknown, unknown>,
   ) {
     super({
       width: cellSize * initialConfig.width + borderSize * 2,
@@ -26,7 +26,7 @@ export class CheckersBoard extends ScreenElement {
     this.graphics.use(graphicState);
   }
 
-  private getStateGroup(initialConfig: GameConfig<any, any, any>) {
+  private getStateGroup(initialConfig: GameConfig<unknown, unknown, unknown>) {
     return new GraphicsGroup({
       members: [
         {
@@ -48,12 +48,12 @@ export class CheckersBoard extends ScreenElement {
         {
           graphic: this.getCells(initialConfig),
           offset: vec(borderSize, borderSize),
-        }
-      ]
+        },
+      ],
     });
   }
 
-  private getVerticalBorder(initialConfig: GameConfig<any, any, any>): GraphicsGroup {
+  private getVerticalBorder(initialConfig: GameConfig<unknown, unknown, unknown>): GraphicsGroup {
     return new GraphicsGroup({
       members: [
         {
@@ -62,34 +62,33 @@ export class CheckersBoard extends ScreenElement {
             height: initialConfig.height * cellSize + borderSize * 2,
             color: lightColor,
           }),
-          offset: vec(0,0)
+          offset: vec(0, 0),
         },
         {
           graphic: new Rectangle({
-            width: borderSize/10,
+            width: borderSize / 10,
             height: initialConfig.height * cellSize + borderSize * 2,
             color: darkColor,
           }),
-          offset: vec(borderSize, borderSize)
+          offset: vec(borderSize, borderSize),
         },
         ...this.getVerticalBorderNumbers(initialConfig),
-      ]
+      ],
     });
   }
 
-  private getVerticalBorderNumbers(initialConfig: GameConfig<any, any, any>): GraphicsGrouping[] {
+  private getVerticalBorderNumbers(initialConfig: GameConfig<unknown, unknown, unknown>): GraphicsGrouping[] {
     return Array(initialConfig.width).fill(0).map<GraphicsGrouping>((_, index) => ({
       graphic: new Text({
         text: String(index + 1),
-        font: PixelFont30px({
-          color: darkColor,
-        })
+        color: darkColor,
+        font: PixelFont30px(),
       }),
       offset: vec(index * cellSize + borderSize, borderSize),
-    }))
+    }));
   }
 
-  private getHorizontalBorder(initialConfig: GameConfig<any, any, any>): GraphicsGroup {
+  private getHorizontalBorder(initialConfig: GameConfig<unknown, unknown, unknown>): GraphicsGroup {
     return new GraphicsGroup({
       members: [
         {
@@ -98,7 +97,7 @@ export class CheckersBoard extends ScreenElement {
             height: borderSize,
             color: lightColor,
           }),
-          offset: vec(0, 0)
+          offset: vec(0, 0),
         },
         {
           graphic: new Rectangle({
@@ -106,38 +105,26 @@ export class CheckersBoard extends ScreenElement {
             height: borderSize / 10,
             color: darkColor,
           }),
-          offset: vec(borderSize, borderSize)
+          offset: vec(borderSize, borderSize),
         },
         ...this.getHorizontalBorderLetters(initialConfig),
-      ]
+      ],
     });
   }
 
-  private getHorizontalBorderLetters(initialConfig: GameConfig<any, any, any>): GraphicsGrouping[] {
+  private getHorizontalBorderLetters(initialConfig: GameConfig<unknown, unknown, unknown>): GraphicsGrouping[] {
     const firstCharIndex = 'A'.charCodeAt(0);
     return Array(initialConfig.height).fill(0).map<GraphicsGrouping>((_, index) => ({
       graphic: new Text({
         text: String.fromCharCode(firstCharIndex + index),
-        // FIXME Cannot read properties of undefined (reading 'toString')
-        //    at FontTextInstance._applyFont (excalibur.js:22153:54)
-        //    at FontTextInstance.measureText (excalibur.js:22085:14)
-        //    at Font.measureTextWithoutCache (excalibur.js:22498:38)
-        //    at FontCache.measureText (excalibur.js:22298:34)
-        //    at Font.measureText (excalibur.js:22508:26)
-        //    at Text._calculateDimension (excalibur.js:22580:45)
-        //    at set text (excalibur.js:22559:14)
-        //    at new Text (excalibur.js:22542:19)
-        //    at eval (CheckersBoard.ts:109:22)
-        //    at Array.map (<anonymous>)
-        font: PixelFont30px({
-          color: darkColor,
-        })
+        color: darkColor,
+        font: PixelFont30px(),
       }),
       offset: vec(borderSize, index * cellSize + borderSize),
-    }))
+    }));
   }
 
-  private getCells(initialConfig: GameConfig<any, any, any>): GraphicsGroup {
+  private getCells(initialConfig: GameConfig<unknown, unknown, unknown>): GraphicsGroup {
     return new GraphicsGroup({
       members: Array(initialConfig.width * initialConfig.height).fill(0).map<GraphicsGrouping>((_, index) => ({
         graphic: new Rectangle({
@@ -146,7 +133,7 @@ export class CheckersBoard extends ScreenElement {
           color: initialConfig.cellMap[index] === 'light' ? lightColor : darkColor,
         }),
         offset: vec((index % initialConfig.width) * cellSize + borderSize, Math.floor(index / initialConfig.width) * cellSize + borderSize),
-      }))
+      })),
     });
   }
 }
