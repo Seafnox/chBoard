4a79c50c642b1f564471b2a88bb906834a128cad
Kirill Warp <kirill9990@gmail.com>
2024-07-21 00:43:33 +0300

    remove context menu

--- src/gui/engine/GameEvent.ts
+++ src/gui/engine/GameEvent.ts
@@ -1,4 +1,4 @@
-import { PointerEvent, Vector } from 'excalibur';
+import { PointerEvent } from 'excalibur';
 import { Unit } from '../../engine/Unit';
 import { SystemActionEvent } from '../events/SystemActionEvent';
 
@@ -6,16 +6,10 @@ export enum GameEvent {
   SystemAction = 'systemaction',
   UnitSelected = 'unitselected',
   PointerDown = 'pointerdown',
-  // TODO Remove useless action
-  AddUnit = 'addunit',
-  // TODO Remove useless action
-  RemoveUnit = 'removeunit',
 }
 
 export interface GameEventMap {
   [GameEvent.PointerDown]: PointerEvent,
   [GameEvent.SystemAction]: SystemActionEvent<unknown>,
   [GameEvent.UnitSelected]: Unit<any, any, any>,
-  [GameEvent.AddUnit]: Vector,
-  [GameEvent.RemoveUnit]: Vector,
 }
