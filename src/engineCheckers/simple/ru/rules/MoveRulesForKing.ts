0bebe130c4b1f1d32ec94850fe770deb1dcc2928
Kirill Warp <kirill9990@gmail.com>
2024-08-25 16:08:19 +0300

    refactor list of king moving position

--- src/engineCheckers/simple/ru/rules/MoveRulesForKing.ts
+++ src/engineCheckers/simple/ru/rules/MoveRulesForKing.ts
@@ -17,33 +17,38 @@ export class MoveRulesForKing extends CheckersRule {
   ): CheckersAvailableAction[] {
     return [
       new KingMoveFrontLeft(game, this, entity, 1),
-      new KingMoveFrontLeft(game, this, entity, 2),
-      new KingMoveFrontLeft(game, this, entity, 3),
-      new KingMoveFrontLeft(game, this, entity, 4),
-      new KingMoveFrontLeft(game, this, entity, 5),
-      new KingMoveFrontLeft(game, this, entity, 6),
-      new KingMoveFrontLeft(game, this, entity, 7),
       new KingMoveFrontRight(game, this, entity, 1),
-      new KingMoveFrontRight(game, this, entity, 2),
-      new KingMoveFrontRight(game, this, entity, 3),
-      new KingMoveFrontRight(game, this, entity, 4),
-      new KingMoveFrontRight(game, this, entity, 5),
-      new KingMoveFrontRight(game, this, entity, 6),
-      new KingMoveFrontRight(game, this, entity, 7),
-
       new KingMoveBackLeft(game, this, entity, 1),
-      new KingMoveBackLeft(game, this, entity, 2),
-      new KingMoveBackLeft(game, this, entity, 3),
-      new KingMoveBackLeft(game, this, entity, 4),
-      new KingMoveBackLeft(game, this, entity, 5),
-      new KingMoveBackLeft(game, this, entity, 6),
-      new KingMoveBackLeft(game, this, entity, 7),
       new KingMoveBackRight(game, this, entity, 1),
+
+      new KingMoveFrontLeft(game, this, entity, 2),
+      new KingMoveFrontRight(game, this, entity, 2),
+      new KingMoveBackLeft(game, this, entity, 2),
       new KingMoveBackRight(game, this, entity, 2),
+
+      new KingMoveFrontLeft(game, this, entity, 3),
+      new KingMoveFrontRight(game, this, entity, 3),
+      new KingMoveBackLeft(game, this, entity, 3),
       new KingMoveBackRight(game, this, entity, 3),
+
+      new KingMoveFrontLeft(game, this, entity, 4),
+      new KingMoveFrontRight(game, this, entity, 4),
+      new KingMoveBackLeft(game, this, entity, 4),
       new KingMoveBackRight(game, this, entity, 4),
+
+      new KingMoveFrontLeft(game, this, entity, 5),
+      new KingMoveFrontRight(game, this, entity, 5),
+      new KingMoveBackLeft(game, this, entity, 5),
       new KingMoveBackRight(game, this, entity, 5),
+
+      new KingMoveFrontLeft(game, this, entity, 6),
+      new KingMoveFrontRight(game, this, entity, 6),
+      new KingMoveBackLeft(game, this, entity, 6),
       new KingMoveBackRight(game, this, entity, 6),
+
+      new KingMoveFrontLeft(game, this, entity, 7),
+      new KingMoveFrontRight(game, this, entity, 7),
+      new KingMoveBackLeft(game, this, entity, 7),
       new KingMoveBackRight(game, this, entity, 7),
     ];
   }
