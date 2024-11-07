7df05cdc7303b0543500dfb781fe4d5c4ee43d68
Kirill Warp <kirill9990@gmail.com>
2024-11-08 00:28:41 +0300

    Refactor: fix codestyle

--- src/engine/actionChanges/TargetActionChange.ts
+++ src/engine/actionChanges/TargetActionChange.ts
@@ -1,6 +1,6 @@
 import {ActionChange} from "./ActionChange";
 
-export interface TargetActionChange extends ActionChange{
+export interface TargetActionChange extends ActionChange {
   targetId: string;
 }
 
